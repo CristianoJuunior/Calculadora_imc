4cd436505127dd0f667f566e09d0833f2deb02a0
Cristiano Notebook <crbjr15@gmail.com>
2023-04-04 15:03:26 -0300

    versao2

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Calculadora de IMC'),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         actions: [
           IconButton(
             onPressed: _resetFields,
